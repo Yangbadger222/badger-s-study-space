5c3d0fae003db56a12129d6970d2653cde4f6074
yangbadger222 <yangbadger222@gmail.com>
2026-09-20 19:23:23 +0800

    fix: hide package branding from install hints

--- web/app/(utility)/settings/document-parsing/page.tsx
+++ web/app/(utility)/settings/document-parsing/page.tsx
@@ -35,9 +35,9 @@ type DocumentParsingPayload = {
 };
 
 const PIP_HINT: Record<string, string> = {
-  docling: "pip install deeptutor[parse-docling]",
-  markitdown: "pip install deeptutor[parse-markitdown]",
-  pymupdf4llm: "pip install deeptutor[parse-pymupdf4llm]",
+  docling: 'pip install -e ".[parse-docling]"',
+  markitdown: 'pip install -e ".[parse-markitdown]"',
+  pymupdf4llm: 'pip install -e ".[parse-pymupdf4llm]"',
 };
 
 export default function DocumentParsingSettingsPage() {
